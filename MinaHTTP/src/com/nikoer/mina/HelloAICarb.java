e63e940f36aa87ec37e557e10262beb1e0dc5131
Sloppy <caozhuhua@gmail.com>
2017-06-04 23:19:39 +0800

    mysql support

--- MinaHTTP/src/com/nikoer/mina/HelloAICarb.java
+++ MinaHTTP/src/com/nikoer/mina/HelloAICarb.java
@@ -1,11 +1,18 @@
 package com.nikoer.mina;
 
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+
 
 /**
  * Servlet implementation class HelloAICarb
@@ -35,8 +42,41 @@ public class HelloAICarb extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String username = request.getParameter("name");
+		if(username!=null){
+			insertUsers(username);
+		}
 		response.getWriter().write("Hello aicarb");
 		response.getWriter().flush();
+	}
+	private void insertUsers(String userName){
+		String user = "root";
+		String password = "s123456";
+		String url = "jdbc:mysql://127.0.0.1:3306/aicarb";
+		String driver = "com.mysql.jdbc.Driver";
+		Connection con = null;
+		Statement stmt = null;
+		try{
+			Class.forName(driver);
+			con = (Connection) DriverManager.getConnection(url, user, password);
+			stmt = (Statement) con.createStatement();
+			String sqlstr = "insert into ac_users(user_name,age) values ('"+userName+"',34)";
+			stmt.executeUpdate(sqlstr);
+		}catch(Exception e){
+			System.out.println(e.toString());
+		}finally{
+			try{
+				if(stmt != null){
+					 stmt.close();
+				 }
+				 if(con != null){
+					 con.close();
+				 }
+			}catch(Exception e){
+				System.out.println(e.toString());
+			}
+			 
+		}
 	}
 
 }
