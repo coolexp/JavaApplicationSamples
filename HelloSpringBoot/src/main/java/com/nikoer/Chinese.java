488749eebb63aef004053e641e1a64e1fe859495
Sloppy <caozhuhua@gmail.com>
2017-07-13 22:58:47 +0800

    spring bean perporty

--- HelloSpringBoot/src/main/java/com/nikoer/Chinese.java
+++ HelloSpringBoot/src/main/java/com/nikoer/Chinese.java
@@ -1,11 +1,20 @@
 package com.nikoer;
 
 public class Chinese implements Human {
+	
+	private String name;
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	@Override
 	public void eat() {
 		// TODO Auto-generated method stub
-		System.out.println("中国人对吃很有一套");
+		System.out.println("中国人对吃很有一套"+this.name);
 	}
 
 	@Override
