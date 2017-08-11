4b92dcdb7f3957d96e7cf605bc5bd390f3f4498b
Sloppy <caozhuhua@gmail.com>
2017-08-11 22:54:14 +0800

    添加jedis

--- HelloSpringBoot/src/main/java/com/nikoer/App.java
+++ HelloSpringBoot/src/main/java/com/nikoer/App.java
@@ -1,5 +1,10 @@
 package com.nikoer;
 
+import java.net.URL;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
@@ -15,6 +20,8 @@ public class App implements EmbeddedServletContainerCustomizer
 {
 	public final static String CHINESE = "Chinese";
 	public final static String AMERICAN = "American";
+
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
